0fa79838604e52a008d12b88703165adf3ea6fdf
André Gonçalves <andreslb1@gmail.com>
2015-12-02 11:56:06 +0100

    TM integration - fix syntax

--- src/bda/plone/cart/browser/__init__.py
+++ src/bda/plone/cart/browser/__init__.py
@@ -235,7 +235,7 @@ def render_cart(context):
     if url.endswith('@@cart') \
        or url.find('@@checkout') != -1 \
        or url.find('@@confirm_order') != -1 \
-       or url.find('/portal_factory/') != -1:
+       or url.find('/portal_factory/') != -1 \
        or url.find('/etickets') != -1 \
        or url.find('@@etickets') != -1:
         return False
